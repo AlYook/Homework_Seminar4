9b373608d9a4599102f66ee608a9dd5600ce5524
Aleksandra Obukh <sem0609@mail.ru>
2022-12-08 20:12:37 +0300

    Solved task 3

---
+++ Homework_3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.WriteLine("Введите массив: ");
+string num = Console.ReadLine();
+
+
+
+Console.Write("[");
+Console.Write(string.Join(", ", num));
+Console.Write("]");
