31223ddfe7453f63e97ecb560f2044baecf43f8b
Aleksandra Obukh <sem0609@mail.ru>
2022-12-08 19:37:48 +0300

    Solved task 2

---
+++ Homework_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Enter num: ");
+int num = int.Parse(Console.ReadLine());
+int sum = 0;
+
+
+for (int temp = 0; num > 0; num /= 10)
+{
+    temp = num % 10;
+    sum = sum + temp;
+}
+Console.WriteLine(sum);
